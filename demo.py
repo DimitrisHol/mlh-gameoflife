111e084932d47d7d881f73a84447351ac57c92f6
george <george030796@gmail.com>
2017-12-02 18:00:02 +0200

    "almost working"

--- demo.py
+++ demo.py
@@ -1,12 +1,18 @@
 import pygame
-
+import copy as copy
 obstacles = []
 #the height of the ground
 height = 0 
 
+
+size_h = 64
+
+size_w = 64
+
 screen = pygame.display.set_mode((600,500))
 
 
+coor = pygame.rect.Rect(300,500-height,64,64)
 
 
 def getObsticale():
@@ -14,37 +20,77 @@ def getObsticale():
 	None
 
 
-def draw():
-	None
+def draw(h,w,image):
+	global coor
+
+	for i in range(0,w):
+		for j in range(0,h):
+
+			if i<=int(h/2) and j<=int(w/2):
+				coord = copy.copy(coor)
+				
+				coor.x -= i*size_w
+				coor.y -= j*size_h
+				screen.blit(image[0], coor)
+				
+				coor = copy.copy(coord)
+
+			elif i<=int(w/2) and j>int(h/2):
+				coord = copy.copy(coor)
+				
+				coor.x -= i*size_w
+				coor.y = j*size_h
+				screen.blit(image[1], coor)
+				
+				coor = copy.copy(coord)
 
 
-def initObstacles(h, image=["octopuss1.png","N.png","facebook.png", "instagram.png"]):
-	global height, obstacles
+			elif i>int(w/2) and j<=int(h/2):
+				coord = copy.copy(coor)
+				
+				coor.x = i*size_w
+				coor.y -= j*size_h
+				screen.blit(image[2], coor)
+				
+				coor = copy.copy(coord)
+
+
+
+			elif i>int(w/2) and j>int(h/2):
+				coord = copy.copy(coor)
+
+				coor.x = i*size_w
+				coor.y = j*size_h
+				screen.blit(image[3], coor)
+
+				coor = copy.copy(coord)
+
+	pygame.display.flip()
+
+
+	if coor.x== 0:
+		coor.x=500
+	coor.move_ip(-1,0)
+
+
+def initObstacles(h, imageStr=["../octopuss1.png","N.png","facebook.png", "instagram.png"]):
+	global height, obstacles, coor
 	height = h
-
-	for i in image:
-		image = pygame.image.load(i)
-
-
-	obstacles.append(pygame.rect.Rect(300,500-height,64,64))
-	print obstacles[0]
-	obstacles[0].move_ip(-3,0)
-	print obstacles[0]
+	image = []
+	for i in imageStr:
+		image.append(pygame.image.load(i))
 
 
 
-	print height
+
+	print len(image)
 	while True:
 		screen.fill((255,0,0))
-		if obstacles[0].x== 0:
-			obstacles[0].x=500
-		obstacles[0].move_ip(-1,0)
-		screen.blit(image, obstacles[0])
 
-		pygame.display.flip()
+		draw(4,4,image)
 
 		pygame.display.update()
 
 
 
-initObstacles(25)
+initObstacles(200)
